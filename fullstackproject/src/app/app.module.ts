85f097ea37d986d4fcbb57386c7c12e348eafdf2
Poojani <poojaniranasinghe2000@gmail.com>
2023-01-16 21:32:08 +0530

    Create components

--- fullstackproject/src/app/app.module.ts
+++ fullstackproject/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BmiComponent } from './bmi/bmi.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { TaskComponent } from './task/task.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeComponent,
+    BmiComponent,
+    WorkoutComponent,
+    TaskComponent,
+    AboutComponent,
+    ContactComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
