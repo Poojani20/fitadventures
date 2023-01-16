415b9ce8abb0c0387eb70f05bbc0c6f5c8c90a66
Poojani <poojaniranasinghe2000@gmail.com>
2023-01-16 21:52:36 +0530

    Config routing

--- fullstackproject/src/app/app-routing.module.ts
+++ fullstackproject/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Pages/about/about.component';
+import { BmiComponent } from './Pages/bmi/bmi.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { FooterComponent } from './Pages/footer/footer.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { TaskComponent } from './Pages/task/task.component';
+import { WelcomeComponent } from './Pages/welcome/welcome.component';
+import { WorkoutComponent } from './Pages/workout/workout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'welcome',pathMatch:"full"},
+  {path:"welcome",component:WelcomeComponent},
+  {path:"header",component:HeaderComponent},
+  {path:"footer",component:FooterComponent},
+  {path:"bmi",component:BmiComponent},
+  {path:"workout",component:WorkoutComponent},
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"login",component:LoginComponent},
+  {path:"task",component:TaskComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fullstackproject/src/app/app.module.ts
+++ fullstackproject/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { BmiComponent } from './bmi/bmi.component';
-import { WorkoutComponent } from './workout/workout.component';
-import { TaskComponent } from './task/task.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './Pages/header/header.component';
+import { FooterComponent } from './Pages/footer/footer.component';
+import { WelcomeComponent } from './Pages/welcome/welcome.component';
+import { BmiComponent } from './Pages/bmi/bmi.component';
+import { WorkoutComponent } from './Pages/workout/workout.component';
+import { TaskComponent } from './Pages/task/task.component';
+import { AboutComponent } from './Pages/about/about.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 @NgModule({
   declarations: [
