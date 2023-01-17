4f83f95747855639b38e5b442dfe49023ce24eb3
Poojani <poojaniranasinghe2000@gmail.com>
2023-01-17 13:10:10 +0530

    Servet get request

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port=process.env.port || 8080 //this is for server port 
 
+app.get('/', (req, res) => {
+    res.send("Welcome here :)")
+})
+
 app.listen(port,()=>{
     console.log("Node server is connected",port)
 })
