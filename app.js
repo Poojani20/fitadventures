6f34e8eec3cd1a7a0b40d4db818abd8b2db02318
Poojani <poojaniranasinghe2000@gmail.com>
2023-01-17 13:24:46 +0530

    Creating models

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port=process.env.port || 8080 //this is for server port 
 
+const authRoute = require('./routes/auth-route');
+
+app.use('/auth',authRoute);
+
 app.get('/', (req, res) => {
     res.send("Welcome here 123 :)")
 })
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,1 +1,20 @@
 const router = require('express').Router();
+
+router.post('/register',(req,res) =>{
+    res.json("Register works")
+});
+
+router.post('/login',(req,res) =>{
+    res.json("Login works")
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = router
