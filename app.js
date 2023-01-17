49d0b4675d3f7694d7d0bb7b9079c44cd87ea928
Poojani <poojaniranasinghe2000@gmail.com>
2023-01-17 13:06:43 +0530

    Creating port

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("This is Poojani")
+const express = require('express');
+
+const app = express();
+
+const port=process.env.port || 8080 //this is for server port 
+
+app.listen(port,()=>{
+    console.log("Node server is connected",port)
+})
